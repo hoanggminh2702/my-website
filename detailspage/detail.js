ec11b9874e99652de5b9d1a247b5c68aede099d0
Hoang Minh <hoanggminh2702@gmail.com>
2022-03-13 02:43:22 +0700

    add increase price via quantity

--- detailspage/detail.js
+++ detailspage/detail.js
@@ -6,6 +6,8 @@ if (!localStorage.getItem("choose-product")) {
   window.location.href = "../mainpage/main.html";
 }
 
+var curQuantity = 1;
+
 const chooseProduct = JSON.parse(localStorage.getItem("choose-product"));
 
 function genProductDetails(product) {
@@ -30,13 +32,13 @@ function genProductDetails(product) {
         <div class="product-price-and-quantity">
           <div class="product-price">
             <h2>Giá sản phẩm</h2>
-            <p>${product.price}$</p>
+            <p>${product.price * curQuantity}$</p>
           </div>
           <div class="product-quantity">
             <h2>Số lượng</h2>
             <div class="control-quantity">
               <div class="control minus">-</div>
-              <div class="quantity">1</div>
+              <div class="quantity">${curQuantity}</div>
               <div class="control plus">+</div>
             </div>
           </div>
@@ -53,16 +55,18 @@ if (chooseProduct) {
   window.location.href = "../mainpage/main.html";
 }
 
-$(".control-quantity").onclick = (e) => {
+$(".main-content").onclick = (e) => {
   if (e.target.closest(".control")) {
-    var curQuantity = Number($(".quantity").innerHTML);
-
     switch (e.target.classList[1]) {
       case "minus":
-        $(".quantity").innerHTML = curQuantity > 1 ? curQuantity - 1 : 1;
+        curQuantity = curQuantity > 1 ? curQuantity - 1 : 1;
+        genProductDetails(chooseProduct);
+
         break;
       case "plus":
-        $(".quantity").innerHTML = curQuantity + 1;
+        curQuantity = curQuantity + 1;
+        genProductDetails(chooseProduct);
+
         break;
       default:
     }
