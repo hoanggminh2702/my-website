e9325f466f161bed3b4e1aff80874a6676bbe7f0
Hoang Minh <hoanggminh2702@gmail.com>
2022-03-12 18:13:31 +0700

    add pagination

--- mainpage/main.js
+++ mainpage/main.js
@@ -3,50 +3,58 @@ const $$ = document.querySelectorAll.bind(document);
 
 const productData = [
   {
+    id: 1,
     link: "https://store.rodbooks.com/Upload/2020/1/31/9-cuon-sach-dan-ong-ban-linh-nhat-dinh-phai-doc-trong-doi-hinh-anh-3.png",
     name: "Sách hay",
     des: "Đây là sách nói về những gì mà chúng tôi hay làm",
     price: 7,
   },
   {
+    id: 2,
     link: "https://store.rodbooks.com/Upload/2020/1/31/9-cuon-sach-dan-ong-ban-linh-nhat-dinh-phai-doc-trong-doi-hinh-anh-4.png",
     name: "Nghĩ và làm giàu",
     des: "Think & Grow Rich - Nghĩ Giàu Và Làm Giàu là một trong những cuốn sách về cách bước chân vào thế giới của Napoleon Hill được bán chạy nhất mọi thời đại. Đã hơn 60 triệu bản được phát hành với gần trăm ngôn ngữ trên toàn thế giới và được công nhận là cuốn sách tạo ra nhiều triệu phú hơn, một cuốn sách truyền cảm hứng thành công nhiều hơn bất cứ cuốn sách kinh doanh nào trong lịch sử.",
     price: 7,
   },
   {
+    id: 3,
     link: "https://store.rodbooks.com/Upload/2020/1/31/9-cuon-sach-dan-ong-ban-linh-nhat-dinh-phai-doc-trong-doi-hinh-anh-5.png",
     name: "Người Giỏi Không Phải Là Người Làm Tất Cả",
     des: "Ngắn gọn, súc tích và đi thẳng vào vấn đề là những gì bạn thấy ngay từ những trang đầu của cuốn sách Người Giỏi Không Phải Là Người Làm Tất Cả này. Từ những người mới bắt đầu tìm hiểu về nghệ thuật quản lý hay đến cả những người đã từng học những khóa học đắt tiền về quản lý cũng sẽ phải gật gù tâm đắc với “Người giỏi không phải là người làm tất cả”. Vì những điều được nói trong cuốn sách thực sự là những điều rất hữu ích và cần thiết nhưng không phải khóa học quản lý nào cũng làm người học hiểu được những điều đó.",
     price: 7,
   },
   {
+    id: 4,
     link: "https://store.rodbooks.com/Upload/2020/1/31/9-cuon-sach-dan-ong-ban-linh-nhat-dinh-phai-doc-trong-doi-hinh-anh-6.png",
     name: "Người Thông Minh Giải Quyết Vấn Đề Như Thế Nào?",
+    des: "Cuốn sách này thực sự đã giúp hàng triệu người trên khắp thế giới tìm ra những giải pháp thành công, ngay cả đối với những vấn đề rắc rối nhất, thử đặt mua và đọc xem, nếu bạn muốn biết cách người thông minh giải quyết vấn đề như thế nào?",
+    price: 7,
+  },
+  {
+    id: 5,
+    link: "https://alphardia.com/wp-content/uploads/2021/12/tu-duy-nhanh-va-cham-tac-gia-daniel-kahneman-1.jpg",
+    name: "Người Thông Minh Giải Quyết Vấn Đề Như Thế Nào?",
+    des: "Tư duy nhanh và chậm”, cuốn sách nổi tiếng tổng hợp tất cả nghiên cứu được tiến hành qua nhiều thập kỷ của nhà tâm lý học từng đạt giải Nobel Kinh tế Daniel Kahneman sẽ cho bạn thấy những sư hợp lý và phi lý trong tư duy của chính bạn",
+    price: 7,
+  },
+  {
+    id: 6,
+    link: "https://product.hstatic.net/200000123069/product/book1_ntmn__1__85927643bab14b0ab9a82d4d3791a141_master.jpg",
+    name: "Người trong muôn nghề - Tủ sách hướng nghiệp",
     des: "Cuốn sách này thực sự đã giúp hàng triệu người trên khắp thế giới tìm ra những giải pháp thành công, ngay cả đối với những vấn đề rắc rối nhất, thử đặt mua và đọc xem, nếu bạn muốn biết cách người thông minh giải quyết vấn đề như thế nào?",
     price: 7,
   },
 ];
 
 // Gen Select
-function genCustomSelectHTMLString(
-  className,
-  menuName,
-  itemNameArr,
-  selector,
-  callbackArr
-) {
+function genCustomSelectHTMLString(className, menuName, itemNameArr) {
   return `
-<div class="select${className ? ` ${className}` : ""}">
+          <div class="select${className ? ` ${className}` : ""}">
             ${menuName}
             <div class="select-options">
 
               ${itemNameArr
                 .map((item, index) => {
-                  if (selector && callbackArr && callbackArr.length)
-                    $(selector).closet(
-                      `.select-option:nth-child(${index})`
-                    ).onclick = callbackArr[index];
                   return `<div class="select-option">${item}</div>`;
                 })
                 .join(" ")}
@@ -73,9 +81,7 @@ const menuArr = [
     menuHTMLString += genCustomSelectHTMLString(
       menu?.className || undefined,
       menu.menuName,
-      menu.itemNameArr,
-      selector,
-      menu?.callbackArr
+      menu.itemNameArr
     );
   });
 
@@ -93,7 +99,7 @@ const productDesLength = 35;
 const productNameLength = 12;
 
 function genProductHTMLString(product) {
-  return `<div class="product-group">
+  return `<div id="product-${product.id}" class="product-group">
     <div class="product-img">
       <img
         src="${product.link}"
@@ -120,11 +126,59 @@ function genProductHTMLString(product) {
   </div>`;
 }
 
-(function genProductList(productList) {
+function genProductList(productList) {
   let productStringHTML = "";
   productList.forEach((product) => {
     productStringHTML += genProductHTMLString(product);
   });
 
   $(".product-container").innerHTML = productStringHTML;
-})(productData);
+}
+
+function renderProductlist(
+  productData,
+  numberOfRecordPerPage = 4,
+  currentPage = 0
+) {
+  const startIndex = currentPage * numberOfRecordPerPage;
+
+  const renderRecord = productData.slice(
+    startIndex,
+    startIndex + numberOfRecordPerPage
+  );
+
+  genProductList(renderRecord);
+}
+
+// Gen pagination
+// Active page = index + 1
+function genPagination(
+  totalProduct,
+  numberOfRecordPerPage = 4,
+  activePage = 0
+) {
+  var totalPage = Math.ceil(totalProduct / numberOfRecordPerPage);
+
+  var paginationString = "";
+
+  for (let i = 0; i < totalPage; i++) {
+    paginationString += `<div id="${i}" class="page${
+      Number(activePage) === i ? " active-page" : ""
+    }">${i + 1}</div>`;
+  }
+
+  $(".pagination-bar").innerHTML = paginationString;
+}
+
+genPagination(productData.length);
+
+renderProductlist(productData);
+
+$(".pagination-bar").onmouseover = (e) => {
+  if (e.target.closest(".page")) {
+    e.target.closest(".page").onclick = (e) => {
+      genPagination(productData.length, 4, e.target.id);
+      renderProductlist(productData, 4, e.target.id);
+    };
+  }
+};
